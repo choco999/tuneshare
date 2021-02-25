bd5c947e6863171eb1c09a3a6af773474691f543
Chisato <49408516+choco999@users.noreply.github.com>
2021-02-24 21:14:07 -0500

    week6 finished

---
+++ delete.php
@@ -0,0 +1,33 @@
+<?php
+ob_start();
+
+$tune_id = filter_input(INPUT_GET, 'id');
+
+try {
+    // connect to db
+    require('connect.php');
+
+    // create a query
+    $sql = "DELETE from songs WHERE user_id = :tune_id;";
+
+    // prepare that query
+    $statement = $db->prepare($sql);
+
+    //bind
+    $statement->bindParam(':tune_id', $tune_id);
+
+    // execute
+    $statement->execute();
+
+    // close the connection
+    $statement->closeCursor();
+    header('location:view.php');
+}
+catch(PDOException $e){
+    header('location:error.php'); 
+}
+
+
+
+ob_flush();
+?>
